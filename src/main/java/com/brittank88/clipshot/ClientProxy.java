60e3cddd696a96ce42cd6c091a3b873a2b689dc4
Mason Hunt <masonhunt130ii@gmail.com>
2024-03-28 00:34:54 +1100

    Version 1.3 - Hopefully fix server crash + update mcmod.info

--- src/main/java/com/brittank88/clipshot/ClientProxy.java
+++ src/main/java/com/brittank88/clipshot/ClientProxy.java
@@ -1,15 +1,9 @@
 package com.brittank88.clipshot;
 
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-
 @SuppressWarnings("unused")
 public class ClientProxy extends CommonProxy {
 
     // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
     // Don't forget to call the super methods as well.
 
-    @Override
-    public void preInit(FMLPreInitializationEvent event) {
-        super.preInit(event);
-    }
 }
--- src/main/java/com/brittank88/clipshot/CommonProxy.java
+++ src/main/java/com/brittank88/clipshot/CommonProxy.java
@@ -11,13 +11,19 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
 
+    private boolean isClient = false;
+
     // preInit "Run before anything else. Read your config, create blocks, items, etc., and register them with the
     // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
 
-        if (event.getSide()
-            .isServer())
+        // Skip initialisation on dedicated servers.
+        isClient = event.getSide()
+            .isClient();
+        if (!isClient) {
             ClipShot.LOG.warn("ClipShot will do nothing on a dedicated server as it is a client-only mod!");
+            return;
+        }
 
         // Initialise the configuration file.
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
@@ -28,6 +34,10 @@ public class CommonProxy {
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(@SuppressWarnings("unused") FMLInitializationEvent event) {
+
+        // Skip initialisation on dedicated servers.
+        if (isClient) return;
+
         ClipShot.LOG.info("Getting system clipboard via AWT...");
 
         try {
